98719bb194861e9934eb21c55c836ff6f8bc6069
Delton Ding <dsh0416@gmail.com>
2016-09-09 11:46:06 +0900

    Init with an example server

--- lib/em-midori.rb
+++ lib/em-midori.rb
@@ -1,5 +1,22 @@
 # encoding: utf-8
+require 'eventmachine'
 
 module EMMidori
+  module Server
+    def receive_data(data)
+      port, ip = Socket.unpack_sockaddr_in(get_peername)
+      puts "from #{ip}:#{port} comes a message:"
+      puts data
+      send_data 'Hello World'
+      close_connection_after_writing
+    end
+  end
 
+  def self.run
+    EventMachine.run {
+      EventMachine.start_server '0.0.0.0', 8081, EMMidori::Server
+    }
+  end
 end
+
+EMMidori.run
