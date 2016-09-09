852e29a360ab0dd15d47c1b9ae0954df8b392935
Delton Ding <dsh0416@gmail.com>
2016-09-09 16:27:55 +0900

    Rearrange the code

--- lib/em-midori.rb
+++ lib/em-midori.rb
@@ -2,21 +2,21 @@
 require 'eventmachine'
 
 module EMMidori
-  module Server
-    def receive_data(data)
-      port, ip = Socket.unpack_sockaddr_in(get_peername)
-      puts "from #{ip}:#{port} comes a message:"
-      puts data
-      send_data 'Hello World'
-      close_connection_after_writing
-    end
-  end
-
-  def self.run
+  def self.run!
     EventMachine.run {
       EventMachine.start_server '0.0.0.0', 8081, EMMidori::Server
     }
   end
+
+  at_exit {EMMidori.run!}
 end
 
-EMMidori.run
+module EMMidori::Server
+  def receive_data(data)
+    port, ip = Socket.unpack_sockaddr_in(get_peername)
+    puts "from #{ip}:#{port} comes a message:"
+    puts data
+    send_data 'Hello World'
+    close_connection_after_writing
+  end
+end
