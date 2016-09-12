f6bf55b3f1f2ff3d3cb70a821835051d04da03e1
Delton Ding <dsh0416@gmail.com>
2016-09-13 03:24:51 +0800

    Use class<<self feature

--- lib/em-midori/api.rb
+++ lib/em-midori/api.rb
@@ -1,145 +1,149 @@
 ##
 # This class provides methods to be inherited as route definition.
 class Midori::API
-  # Add GET method as a DSL for route definition
-  # === Attributes
-  # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
-  # === Returns
-  # nil
-  # === Examples
-  # String as router
-  #   get '/' do
-  #      puts 'Hello World'
-  #   end
-  #
-  # Regex as router
-  #   get /\/hello\/(.*?)/ do
-  #      puts 'Hello World'
-  #   end
-  def self.get(path, &block) end
+  class << self
+    # Add GET method as a DSL for route definition
+    # === Attributes
+    # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
+    # === Returns
+    # nil
+    # === Examples
+    # String as router
+    #   get '/' do
+    #      puts 'Hello World'
+    #   end
+    #
+    # Regex as router
+    #   get /\/hello\/(.*?)/ do
+    #      puts 'Hello World'
+    #   end
+    def get(path, &block) end
 
-  # Add POST method as a DSL for route definition
-  # === Attributes
-  # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
-  # === Returns
-  # nil
-  # === Examples
-  # String as router
-  #   post '/' do
-  #      puts 'Hello World'
-  #   end
-  #
-  # Regex as router
-  #   post /\/hello\/(.*?)/ do
-  #      puts 'Hello World'
-  #   end
-  def self.post(path, &block) end
+    # Add POST method as a DSL for route definition
+    # === Attributes
+    # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
+    # === Returns
+    # nil
+    # === Examples
+    # String as router
+    #   post '/' do
+    #      puts 'Hello World'
+    #   end
+    #
+    # Regex as router
+    #   post /\/hello\/(.*?)/ do
+    #      puts 'Hello World'
+    #   end
+    def post(path, &block) end
 
-  # Add PUT method as a DSL for route definition
-  # === Attributes
-  # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
-  # === Returns
-  # nil
-  # === Examples
-  # String as router
-  #   put '/' do
-  #      puts 'Hello World'
-  #   end
-  #
-  # Regex as router
-  #   put /\/hello\/(.*?)/ do
-  #      puts 'Hello World'
-  #   end
-  def self.put(path, &block) end
+    # Add PUT method as a DSL for route definition
+    # === Attributes
+    # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
+    # === Returns
+    # nil
+    # === Examples
+    # String as router
+    #   put '/' do
+    #      puts 'Hello World'
+    #   end
+    #
+    # Regex as router
+    #   put /\/hello\/(.*?)/ do
+    #      puts 'Hello World'
+    #   end
+    def put(path, &block) end
 
-  # Add DELETE method as a DSL for route definition
-  # === Attributes
-  # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
-  # === Returns
-  # nil
-  # === Examples
-  # String as router
-  #   delete '/' do
-  #      puts 'Hello World'
-  #   end
-  #
-  # Regex as router
-  #   delete /\/hello\/(.*?)/ do
-  #      puts 'Hello World'
-  #   end
-  def self.delete(path, &block) end
+    # Add DELETE method as a DSL for route definition
+    # === Attributes
+    # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
+    # === Returns
+    # nil
+    # === Examples
+    # String as router
+    #   delete '/' do
+    #      puts 'Hello World'
+    #   end
+    #
+    # Regex as router
+    #   delete /\/hello\/(.*?)/ do
+    #      puts 'Hello World'
+    #   end
+    def delete(path, &block) end
 
-  # Add OPTIONS method as a DSL for route definition
-  # === Attributes
-  # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
-  # === Returns
-  # nil
-  # === Examples
-  # String as router
-  #   options '/' do
-  #      puts 'Hello World'
-  #   end
-  #
-  # Regex as router
-  #   options /\/hello\/(.*?)/ do
-  #      puts 'Hello World'
-  #   end
-  def self.options(path, &block) end
+    # Add OPTIONS method as a DSL for route definition
+    # === Attributes
+    # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
+    # === Returns
+    # nil
+    # === Examples
+    # String as router
+    #   options '/' do
+    #      puts 'Hello World'
+    #   end
+    #
+    # Regex as router
+    #   options /\/hello\/(.*?)/ do
+    #      puts 'Hello World'
+    #   end
+    def options(path, &block) end
 
-  # Add LINK method as a DSL for route definition
-  # === Attributes
-  # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
-  # === Returns
-  # nil
-  # === Examples
-  # String as router
-  #   link '/' do
-  #      puts 'Hello World'
-  #   end
-  #
-  # Regex as router
-  #   link /\/hello\/(.*?)/ do
-  #      puts 'Hello World'
-  #   end
-  def self.link(path, &block) end
+    # Add LINK method as a DSL for route definition
+    # === Attributes
+    # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
+    # === Returns
+    # nil
+    # === Examples
+    # String as router
+    #   link '/' do
+    #      puts 'Hello World'
+    #   end
+    #
+    # Regex as router
+    #   link /\/hello\/(.*?)/ do
+    #      puts 'Hello World'
+    #   end
+    def link(path, &block) end
 
-  # Add UNLINK method as a DSL for route definition
-  # === Attributes
-  # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
-  # === Returns
-  # nil
-  # === Examples
-  # String as router
-  #   unlink '/' do
-  #      puts 'Hello World'
-  #   end
-  #
-  # Regex as router
-  #   unlink /\/hello\/(.*?)/ do
-  #      puts 'Hello World'
-  #   end
-  def self.unlink(path, &unlink) end
+    # Add UNLINK method as a DSL for route definition
+    # === Attributes
+    # * +path+ [+String+, +Regex+] - Accepts as part of path in route definition.
+    # === Returns
+    # nil
+    # === Examples
+    # String as router
+    #   unlink '/' do
+    #      puts 'Hello World'
+    #   end
+    #
+    # Regex as router
+    #   unlink /\/hello\/(.*?)/ do
+    #      puts 'Hello World'
+    #   end
+    def unlink(path, &unlink) end
 
-  METHODS = %w'get post put delete options link unlink' # :nodoc:
+    METHODS = %w'get post put delete options link unlink' # :nodoc:
 
-  # Magics to fill DSL methods through dynamically class method definition
-  METHODS.each do |method|
-    define_singleton_method(method) do |*args|
-      add_route(method.upcase, args[0], args[1]) # args[0]: route, # args[1]: block
+    # Magics to fill DSL methods through dynamically class method definition
+    METHODS.each do |method|
+      define_singleton_method(method) do |*args|
+        add_route(method.upcase, args[0], args[1]) # args[0]: route, # args[1]: block
+      end
     end
-  end
 
-  def self.add_route(method, path, block)
-    @route = Array.new if @route.nil?
-    @route << Midori::Route.new(method, path, block)
-    nil
+    def add_route(method, path, block)
+      @route = Array.new if @route.nil?
+      @route << Midori::Route.new(method, path, block)
+      nil
+    end
+
+    # def match(method, route)
+    #
+    # end
+
   end
 
   private_class_method :add_route
 
-  # def self.match(method, route)
-  #
-  # end
 end
 
 class Midori::Route
