5198c2ab9cdf291e4700e7b6236f8e5c0780a6bc
Delton Ding <dsh0416@gmail.com>
2016-09-13 03:09:51 +0800

    Fix fake routes should be class methods but not instance methods (for static analysis use and doc generation)

--- lib/em-midori/api.rb
+++ lib/em-midori/api.rb
@@ -16,7 +16,7 @@ class Midori::API
   #   get /\/hello\/(.*?)/ do
   #      puts 'Hello World'
   #   end
-  def get(path, &block) end
+  def self.get(path, &block) end
 
   # Add POST method as a DSL for route definition
   # === Attributes
@@ -33,7 +33,7 @@ class Midori::API
   #   post /\/hello\/(.*?)/ do
   #      puts 'Hello World'
   #   end
-  def post(path, &block) end
+  def self.post(path, &block) end
 
   # Add PUT method as a DSL for route definition
   # === Attributes
@@ -50,7 +50,7 @@ class Midori::API
   #   put /\/hello\/(.*?)/ do
   #      puts 'Hello World'
   #   end
-  def put(path, &block) end
+  def self.put(path, &block) end
 
   # Add DELETE method as a DSL for route definition
   # === Attributes
@@ -67,7 +67,7 @@ class Midori::API
   #   delete /\/hello\/(.*?)/ do
   #      puts 'Hello World'
   #   end
-  def delete(path, &block) end
+  def self.delete(path, &block) end
 
   # Add OPTIONS method as a DSL for route definition
   # === Attributes
@@ -84,7 +84,7 @@ class Midori::API
   #   options /\/hello\/(.*?)/ do
   #      puts 'Hello World'
   #   end
-  def options(path, &block) end
+  def self.options(path, &block) end
 
   # Add LINK method as a DSL for route definition
   # === Attributes
@@ -101,7 +101,7 @@ class Midori::API
   #   link /\/hello\/(.*?)/ do
   #      puts 'Hello World'
   #   end
-  def link(path, &block) end
+  def self.link(path, &block) end
 
   # Add UNLINK method as a DSL for route definition
   # === Attributes
@@ -118,7 +118,7 @@ class Midori::API
   #   unlink /\/hello\/(.*?)/ do
   #      puts 'Hello World'
   #   end
-  def unlink(path, &unlink) end
+  def self.unlink(path, &unlink) end
 
   METHODS = %w'get post put delete options link unlink' # :nodoc:
 
@@ -129,12 +129,13 @@ class Midori::API
     end
   end
 
-  private
   def self.add_route(method, path, block)
     @route = Array.new if @route.nil?
     @route << Midori::Route.new(method, path, block)
     nil
   end
+
+  private_class_method :add_route
 
   # def self.match(method, route)
   #
@@ -144,9 +145,9 @@ end
 class Midori::Route
   attr_accessor :method, :path, :function
 
-  def initialize(method, route, function)
+  def initialize(method, path, function)
     @method = method
-    path = path
+    @path = path
     @function = function
   end
 end
