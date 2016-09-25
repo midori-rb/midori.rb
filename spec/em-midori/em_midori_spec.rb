7ed3366d71c47dbaae5ced403a213b0d395a93e6
Delton Ding <dsh0416@gmail.com>
2016-09-25 11:41:34 +0800

    Add more tests

--- spec/em-midori/em_midori_spec.rb
+++ spec/em-midori/em_midori_spec.rb
@@ -27,6 +27,10 @@ RSpec.describe Midori do
       expect(Net::HTTP.get(URI('http://127.0.0.1:8080/'))).to eq('Hello World')
     end
 
+    it 'should return 404 Not Found on GET /not_found_error' do
+      expect(Net::HTTP.get(URI('http://127.0.0.1:8080/not_found_error'))).to eq('404 Not Found')
+    end
+
     it 'should stop properly' do
       expect(Midori.stop).to eq(true)
       sleep(1)
