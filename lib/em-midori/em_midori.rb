2249cd64e34f9bef35e6f7316e89bb7fe1c38159
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-10-20 15:56:14 -0200

    Add Logger instead of using puts

--- lib/em-midori/em_midori.rb
+++ lib/em-midori/em_midori.rb
@@ -1,21 +1,26 @@
+require 'logger'
+
 module Midori
-  def self.run(api = Midori::API, ip = nil, port = nil)
+  @@logger = ::Logger.new(StringIO.new)
+
+  def self.run(api = Midori::API, ip = nil, port = nil, logger = ::Logger.new(STDOUT))
     ip ||= '127.0.0.1'
     port ||= 8081
+    @@logger = logger
     EventMachine.run do
-      puts "Midori #{Midori::VERSION} is now running on #{ip}:#{port}".blue
-      @midori_server = EventMachine.start_server ip, port, Midori::Server, api
+      @@logger.info "Midori #{Midori::VERSION} is now running on #{ip}:#{port}".blue
+      @midori_server = EventMachine.start_server ip, port, Midori::Server, api, logger
     end
   end
 
   def self.stop
     if @midori_server.nil?
-      puts 'Midori Server has NOT been started'.red
+      @@logger.error 'Midori Server has NOT been started'.red
       return false
     else
       EventMachine.stop_server(@midori_server)
       @midori_server = nil
-      puts 'Goodbye Midori'.blue
+      @@logger.info 'Goodbye Midori'.blue
       return true
     end
   end
