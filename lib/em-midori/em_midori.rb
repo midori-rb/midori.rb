14f5dd03bf4ceccfcd5b5340286ea81fc7b9353c
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-10-20 18:02:39 -0200

    Store logger on instance variable

--- lib/em-midori/em_midori.rb
+++ lib/em-midori/em_midori.rb
@@ -1,26 +1,26 @@
 require 'logger'
 
 module Midori
-  @@logger = ::Logger.new(StringIO.new)
+  @logger = ::Logger.new(StringIO.new)
 
   def self.run(api = Midori::API, ip = nil, port = nil, logger = ::Logger.new(STDOUT))
     ip ||= '127.0.0.1'
     port ||= 8081
-    @@logger = logger
+    @logger = logger
     EventMachine.run do
-      @@logger.info "Midori #{Midori::VERSION} is now running on #{ip}:#{port}".blue
+      @logger.info "Midori #{Midori::VERSION} is now running on #{ip}:#{port}".blue
       @midori_server = EventMachine.start_server ip, port, Midori::Server, api, logger
     end
   end
 
   def self.stop
     if @midori_server.nil?
-      @@logger.error 'Midori Server has NOT been started'.red
+      @logger.error 'Midori Server has NOT been started'.red
       return false
     else
       EventMachine.stop_server(@midori_server)
       @midori_server = nil
-      @@logger.info 'Goodbye Midori'.blue
+      @logger.info 'Goodbye Midori'.blue
       return true
     end
   end
