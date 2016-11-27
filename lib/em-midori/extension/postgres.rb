133588567bf3b3cd6e50670d71f27d91e85ab781
Delton Ding <dsh0416@gmail.com>
2016-11-28 00:59:48 +0800

    Add connect and query method

--- lib/em-midori/extension/postgres.rb
+++ lib/em-midori/extension/postgres.rb
@@ -4,25 +4,18 @@ class Midori::Postgres
   end
 
   def connect(dbname, username, password)
-    Promise.new(->(resolve, _reject) {
+    await(Promise.new(->(resolve, _reject) {
       @db.connect(dbname, username, password).callback do |status|
         resolve.call(status)
       end
-    })
+    }))
+  end
+
+  def query(str)
+    await(Promise.new(->(resolve, _reject) {
+      @db.query(str).callback do |status, result, errors|
+        resolve.call([status, result, errors])
+      end
+    }))
   end
 end
-
-# EM.run do
-#   db = EM.connect_unix_domain( "/tmp/.s.PGSQL.5432", EM::P::Postgres3 ) 
-#   db.connect( dbname, username, psw ).callback do |status| 
-#     if status 
-#       db.query( "select * from some_table" ).callback do |status, result, errors| 
-#         if status 
-#           result.rows.each do |row| 
-#             p row 
-#           end 
-#         end 
-#       end 
-#     end 
-#   end 
-# end
