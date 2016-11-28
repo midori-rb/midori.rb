ab70308b25e8d40597b8908e448e2f64fbdaf14a
Delton Ding <dsh0416@gmail.com>
2016-11-28 15:32:20 +0800

    Test query process

--- spec/em-midori/postgres_spec.rb
+++ spec/em-midori/postgres_spec.rb
@@ -8,13 +8,19 @@ RSpec.describe Midori::Postgres do
       async :test_postgres do
         @db = Midori::Postgres.new('127.0.0.1', 5432)
         answer << @db.connect('travis_ci_test', 'postgres', '')
+        answer << (@db.query <<-SQL
+          CREATE TABLE IF NOT EXISTS foo (
+            bar varchar(45)
+          )
+        SQL
+        ).cmd_tag
         EM.stop
       end
       EM.run {
         test_postgres
         answer << 0
       }
-      expect(answer).to eq([0, true])
+      expect(answer).to eq([0, true, 'CREATE TABLE'])
     end
   end
 end
