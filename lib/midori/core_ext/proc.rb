bf2a595adfeb501e1534044106992daab6b9d592
Delton Ding <dsh0416@gmail.com>
2016-12-21 20:47:25 +0800

    Fix syntax

--- lib/midori/core_ext/proc.rb
+++ lib/midori/core_ext/proc.rb
@@ -1,5 +1,5 @@
 class Proc
-  # @note Converting {Proc} to {Lambda} may have incorrect behaviours on corner cases.
+  # @note Converting [Proc] to [Lambda] may have incorrect behaviours on corner cases.
   # @note See {Ruby Language Issues}[https://bugs.ruby-lang.org/issues/7314] for more details.
   def to_lambda (instance = Object.new)
     instance.define_singleton_method(:_, &self)
