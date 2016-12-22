8783deb7c5ccb7d716e123bca8d416f78d737008
Delton Ding <dsh0416@gmail.com>
2016-12-22 14:08:43 +0800

    Enrich document in PromiseException

--- lib/midori/core_ext/promise_exception.rb
+++ lib/midori/core_ext/promise_exception.rb
@@ -1,3 +1,6 @@
+##
+# A special error as containers of errors inside [ Promise ]
+# @attr [ StandardError ] raw_exeption raw execption raised
 class PromiseException < StandardError
   attr_reader :raw_exception
   def initialize(raw_exception)
