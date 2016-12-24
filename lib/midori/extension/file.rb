4880c8f8f3d1b15b14ff9078c6c04a3bb797e9f6
Delton Ding <dsh0416@gmail.com>
2016-12-24 23:23:52 +0800

    Improve doc

--- lib/midori/extension/file.rb
+++ lib/midori/extension/file.rb
@@ -3,11 +3,13 @@
 class Midori::File
   class << self
     # Same APIs as File.read
+    # @param [ Array ] args args of File.read
     def read(*args)
       await(defer{File.read(*args)})
     end
 
     # Same APIs as File.write
+    # @param [ Array ] args args of File.write
     def write(*args)
       await(defer{File.write(*args)})
     end
