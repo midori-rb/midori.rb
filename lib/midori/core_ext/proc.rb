6a96e1bd3408bfb970d1c785675e0dcc43e960c3
Delton Ding <dsh0416@gmail.com>
2017-01-11 15:37:16 +0800

    Fix code style

--- lib/midori/core_ext/proc.rb
+++ lib/midori/core_ext/proc.rb
@@ -1,12 +1,12 @@
 ##
 # Meta-programming Proc for Syntactic Sugars
 class Proc
-  # Convert [ Proc ] to [ Lambda ]
-  # @param [ Object ] instance the context
-  # @return [ Lambda ] Lambda converted
+  # Convert [Proc] to [Lambda]
+  # @param [Object] instance the context
+  # @return [Lambda] Lambda converted
   # @note Converting [Proc] to [Lambda] may have incorrect behaviours on corner cases.
   # @note See {Ruby Language Issues}[https://bugs.ruby-lang.org/issues/7314] for more details.
-  def to_lambda (instance = Object.new)
+  def to_lambda(instance = Object.new)
     instance.define_singleton_method(:_, &self)
     instance.method(:_).to_proc
   end
