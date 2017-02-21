93c2d80c8f584f60c0e5029cb84af750199e98a9
Delton Ding <dsh0416@gmail.com>
2017-02-21 15:52:31 +0800

    Simplify code

--- lib/midori/connection.rb
+++ lib/midori/connection.rb
@@ -30,11 +30,7 @@ class Midori::Connection
   end
 
   def send_data(data)
-    if @monitor.writable?
-      @socket.write_nonblock(data)
-    else
-      @data << data
-    end
+    @monitor.writable? ? @socket.write_nonblock(data) : @data << data
   end
 
   def close_connection
