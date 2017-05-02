1a98f5214132b9520a0fb9090e081b59682478d5
Delton Ding <dsh0416@gmail.com>
2017-05-02 22:40:06 +0800

    Make eventloop configurable

--- lib/midori/core_ext/event_loop.rb
+++ lib/midori/core_ext/event_loop.rb
@@ -2,21 +2,26 @@
 # EventLoop Module, providing main loop for events
 module EventLoop
   class << self
-    # Raw NIO Selector
-    SELECTOR = NIO::Selector.new
-    # Array of active timers
-    TIMERS = []
-    # Hash of io and its callback
-    IOS = Hash.new
-    # IO queue
-    QUEUE = Hash.new
+    # Config EvnetLoop, call by default if any other methods called
+    # @param [NIO::Selector] selector an event selector
+    def config(selector = NIO::Selector.new)
+      # Raw NIO Selector
+      @selector = selector
+      # Array of active timers
+      @timers = []
+      # Hash of io and its callback
+      @ios = Hash.new
+      # IO queue
+      @queue = Hash.new
+    end
 
     # Add timer in event loop
     # @param [EventLoop::Timer] timer timer to insert
     # @return [nil] nil
     def add_timer(timer)
+      config if @selector.nil?
       timer.start_time = Time.now.to_f + timer.time
-      TIMERS << timer
+      @timers << timer
       nil
     end
 
@@ -26,11 +31,11 @@ module EventLoop
     # @yield what to run when io callbacks
     # @return [nil] nil
     def register(io, interest=(:rw), &callback)
-      if QUEUE[io.to_i].nil?
-        QUEUE[io.to_i] = Array.new
+      if @queue[io.to_i].nil?
+        @queue[io.to_i] = Array.new
         register_raw(io, interest, callback)
       else
-        QUEUE[io.to_i] << [io, interest, callback]
+        @queue[io.to_i] << [io, interest, callback]
       end
       nil
     end
@@ -41,8 +46,9 @@ module EventLoop
     # @param [Proc] callback what to run when io callbacks
     # @return [nil] nil
     def register_raw(io, interest=(:rw), callback)
-      SELECTOR.register(io, interest)
-      IOS[io] = { callback: callback }
+      config if @selector.nil?
+      @selector.register(io, interest)
+      @ios[io] = { callback: callback }
       nil
     end
 
@@ -51,28 +57,30 @@ module EventLoop
     # @return [nil] nil
     def deregister(io)
       fd = io.to_i
-      SELECTOR.deregister(io)
-      IOS.delete(io)
-      next_register = QUEUE[fd].shift
-      next_register.nil? ? QUEUE.delete(fd) : register_raw(*next_register)
+      @selector.deregister(io)
+      @ios.delete(io)
+      next_register = @queue[fd].shift
+      next_register.nil? ? @queue.delete(fd) : register_raw(*next_register)
       nil
     end
 
     # Run I/O selector once
     # @return [nil] nil
     def run_once
-      SELECTOR.select(0.2) do |monitor| # Timeout for 0.2 secs
-        IOS[monitor.io][:callback].call(monitor)
+      config if @selector.nil?
+      @selector.select(0.2) do |monitor| # Timeout for 0.2 secs
+        @ios[monitor.io][:callback].call(monitor)
       end
-      EventLoop.timer_once
+      timer_once
       nil
     end
 
     # Run timer once
     # @return [nil] nil
     def timer_once
+      config if @selector.nil?
       now_time = Time.now.to_f
-      TIMERS.delete_if do |timer|
+      @timers.delete_if do |timer|
         if timer.start_time < now_time
           timer.callback.call
           true
