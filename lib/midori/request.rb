aee91a6326b06dde06fdca5a02a66b008974a24d
Delton Ding <dsh0416@gmail.com>
2017-06-04 22:35:07 +0800

    Fix Content-Length header detection

--- lib/midori/request.rb
+++ lib/midori/request.rb
@@ -52,7 +52,7 @@ class Midori::Request
     end
 
     # Set body parsed if body reaches content length
-    if (@header['Content-Length'].to_i || 0) == @body.bytesize
+    if (@header['Content-Length'].to_i || 0) <= @body.bytesize
       @body_parsed = true
       pre_proceed
     end
