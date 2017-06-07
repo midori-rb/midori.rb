16b1c8fc949a6ae29ff224fffd5b8fee8e23fed2
Delton Ding <dsh0416@gmail.com>
2017-06-07 10:45:43 +0800

    Add document

--- lib/midori/env.rb
+++ lib/midori/env.rb
@@ -1,14 +1,17 @@
 module Midori
+  # @return [String] midori environment
   def self.env
     ENV['MIDORI_ENV'] || 'development'
   end
 end
 
 class String
+  # @return [TrueClass | FalseClass] if string is equal to production
   def production?
     self == 'production'
   end
 
+  #@return [TrueClass | FalseClass] if string is equal to development
   def development?
     self == 'development'
   end
