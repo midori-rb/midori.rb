982f22f79470b2d8f738240aebcbd05439468cf9
Delton Ding <dsh0416@gmail.com>
2017-06-15 09:51:00 +0800

    Remove extensions

--- spec/midori/safe_require_spec.rb
+++
@@ -1,9 +0,0 @@
-require './spec/spec_helper'
-
-RSpec.describe Kernel do
-  describe 'safe require' do
-    it 'raise prompt when file not found' do
-      expect {safe_require 'UNKNOWN_FILE', 'prompt'}.to raise_error('prompt')
-    end
-  end
-end
