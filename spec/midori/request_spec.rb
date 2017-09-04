1bbd793a97b5720b35c8d179e0883f714ca77253
Delton Ding <dsh0416@gmail.com>
2017-09-04 11:15:00 +0800

    Rename request_param to request_params, change default value from nil to empty Hash

--- spec/midori/request_spec.rb
+++ spec/midori/request_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Midori::Request do
       expect(request.method).to eq(:GET)
       expect(request.path).to eq('/')
       expect(request.query_string).to eq(nil)
-      expect(request.query_param).to eq(nil)
+      expect(request.query_params).to eq({})
     end
 
     it 'parse request with query_string' do
@@ -21,7 +21,7 @@ RSpec.describe Midori::Request do
       expect(request.method).to eq(:GET)
       expect(request.path).to eq('/')
       expect(request.query_string).to eq('test=1')
-      expect(request.query_param['test']).to eq(['1'])
+      expect(request.query_params['test']).to eq(['1'])
     end
   end
 
