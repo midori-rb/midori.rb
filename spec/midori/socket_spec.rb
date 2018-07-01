03c653827b77b6a74fe8e282c8cb977a3dbeabde
Delton Ding <dsh0416@gmail.com>
2018-07-01 13:49:44 +0800

    Add spec

--- spec/midori/socket_spec.rb
+++ spec/midori/socket_spec.rb
@@ -12,5 +12,14 @@ RSpec.describe Socket do
       # You cannot set it again when socket's closed
       expect(socket.tcp_fast_open).to eq(false)
     end
+
+    it 'should use TCP reuseport feature' do
+      socket = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
+      socket.reuse_port if Midori::Configure.socket_reuse_port
+      socket.bind Addrinfo.tcp '127.0.0.1', 8082
+      socket.listen Socket::SOMAXCONN
+      expect(socket.reuse_port).to eq(true)
+      socket.close
+    end
   end
 end
