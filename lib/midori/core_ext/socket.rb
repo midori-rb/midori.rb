8601e8b1cfc7e5ba1e116494d160a7c37a78aea5
Delton Ding <dsh0416@gmail.com>
2018-07-02 11:24:06 +0800

    Remove `self`s

--- lib/midori/core_ext/socket.rb
+++ lib/midori/core_ext/socket.rb
@@ -3,14 +3,14 @@ class Socket
     # macOS devices option is DIFFERENT from Linux and FreeBSD
     opt = (/darwin/ =~ RUBY_PLATFORM) ? 1 : 5
     # Magic number 6 may refer to Socket::SOL_TCP or Socket::IPPROTO_TCP
-    self.setsockopt(6, Socket::TCP_FASTOPEN, opt)
+    setsockopt(6, Socket::TCP_FASTOPEN, opt)
     true
   rescue => _e
     false
   end
 
   def reuse_port
-    self.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEPORT, 1)
+    setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEPORT, 1)
     true
   rescue => _e
     false
