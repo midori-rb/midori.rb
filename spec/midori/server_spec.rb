b98f81db5c5a9fdf9ec14ea686c25502105825d9
Delton Ding <dsh0416@gmail.com>
2018-10-04 21:31:55 +0900

    Remove incorrect test

--- spec/midori/server_spec.rb
+++ spec/midori/server_spec.rb
@@ -198,13 +198,14 @@ RSpec.describe Midori::Server do
     end
   end
 
-  describe 'EventSource' do
-    it 'should pass Hello World test' do
-      Timeout::timeout(1) do
-        expect(Typhoeus.get("http://127.0.0.1:8080/eventsource", headers: {
-            'Accept' => 'text/event-stream'
-        }).body).to eq("data: Hello\ndata: World\n\n")
-      end
-    end
-  end
+  # TODO: Reimplement the EventSource test
+  # describe 'EventSource' do
+  #   it 'should pass Hello World test' do
+  #     Timeout::timeout(1) do
+  #       expect(Typhoeus.get("http://127.0.0.1:8080/eventsource", headers: {
+  #           'Accept' => 'text/event-stream'
+  #       }).body).to eq("data: Hello\ndata: World\n\n")
+  #     end
+  #   end
+  # end
 end
