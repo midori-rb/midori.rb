fae0c8ecc56a45cb7f9182fac0a4fc6c3aa541df
Delton Ding <dsh0416@gmail.com>
2019-07-28 00:03:17 +0800

    Fix frame size

--- lib/midori/websocket.rb
+++ lib/midori/websocket.rb
@@ -32,11 +32,27 @@ class Midori::WebSocket
   # @param [Array<Integer>, String] msg data to send
   def send(msg)
     output = []
+    payload_length = []
+    if msg.size < 126
+      payload_length << msg.size
+    elsif msg.size < 65_536
+      payload_length << 126
+      payload_length.concat([msg.size].pack('n').unpack('C*'))
+    elsif msg.size < 2**63
+      payload_length << 127
+      payload_length.concat([msg.size >> 32, msg.size & 0xFFFFFFFF].pack('NN').unpack('C*'))
+    else
+      raise Midori::Exception::ContinuousFrame
+    end
+
     if msg.is_a?String
-      output << 0b10000001 << msg.size << msg
-      @connection.send_data(output.pack("CCA#{msg.size}"))
+      output << 0b10000001
+      output.concat payload_length
+      output.concat msg.unpack('C*')
+      @connection.send_data(output.pack('C*'))
     elsif msg.is_a? Array
-      output << 0b10000010 << msg.size
+      output << 0b10000010
+      output.concat payload_length
       output.concat msg
       @connection.send_data(output.pack('C*'))
     else
